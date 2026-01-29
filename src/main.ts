cd1d0303aeee4fdcd14c4fce41b21f6717565f22
Kanat Sydykov <ksydykov@nurtelecom.kg>
2026-01-29 12:08:25 +0600

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'https://cloud.sydykov.com',
+    credentials: true
+  });
+
   await app.listen(process.env.PORT ?? 8000);
 }
 
